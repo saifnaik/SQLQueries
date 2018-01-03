0a80252ed6a8648a55057eb4b8251cdc9f375c04
Saif Naik <saif.naik@fastfilmz.com>
2018-01-03 15:54:00 +0530

    Basic Queries
    Basic content queries.

---
+++ ContentQueries.sql
@@ -0,0 +1,36 @@
+#Count of the top movies watched
+
+SELECT COUNT(*),a_contentname
+    from awsma.event
+    where event_type in ('MovieViewStart','ContentViewStart')
+    and a_platform in ('Android','ANDROID')
+    and a_subscriberid is not null
+    and arrival_timestamp AT TIME ZONE 'UTC' BETWEEN SYSDATE - INTERVAL '60 DAY' AND SYSDATE
+    and a_browsinglanguage='tamil'
+GROUP BY a_contentname
+ORDER BY COUNT DESC
+
+#Count of the top scenes watched
+
+SELECT COUNT(*),a_contentname
+    from awsma.event
+    where event_type in ('SceneViewStart')
+    and a_platform in ('Android','ANDROID')
+    and a_subscriberid is not null
+    and arrival_timestamp AT TIME ZONE 'UTC' BETWEEN SYSDATE - INTERVAL '60 DAY' AND SYSDATE
+    and a_browsinglanguage='tamil'
+GROUP BY a_contentname
+ORDER BY COUNT DESC
+
+#Count of the top songs watched
+
+SELECT COUNT(*),a_contentname
+    from awsma.event
+    where event_type in ('SongViewStart')
+    and a_platform in ('Android','ANDROID')
+    and a_subscriberid is not null
+    and arrival_timestamp AT TIME ZONE 'UTC' BETWEEN SYSDATE - INTERVAL '60 DAY' AND SYSDATE
+    and a_browsinglanguage='tamil'
+GROUP BY a_contentname
+ORDER BY COUNT DESC
+
