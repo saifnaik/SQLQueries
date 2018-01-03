4aae5f501fc041e80e1f6df34f1993893099a20d
Saif Naik <saif.naik@fastfilmz.com>
2018-01-03 16:03:09 +0530

    Small change
    Need last 6 months data

--- ContentQueries.sql
+++ ContentQueries.sql
@@ -5,7 +5,7 @@ SELECT COUNT(*),a_contentname
     where event_type in ('MovieViewStart','ContentViewStart')
     and a_platform in ('Android','ANDROID')
     and a_subscriberid is not null
-    and arrival_timestamp AT TIME ZONE 'UTC' BETWEEN SYSDATE - INTERVAL '60 DAY' AND SYSDATE
+    and arrival_timestamp AT TIME ZONE 'UTC' BETWEEN SYSDATE - INTERVAL '180 DAY' AND SYSDATE
     and a_browsinglanguage='tamil'
 GROUP BY a_contentname
 ORDER BY COUNT DESC
@@ -17,7 +17,7 @@ SELECT COUNT(*),a_contentname
     where event_type in ('SceneViewStart')
     and a_platform in ('Android','ANDROID')
     and a_subscriberid is not null
-    and arrival_timestamp AT TIME ZONE 'UTC' BETWEEN SYSDATE - INTERVAL '60 DAY' AND SYSDATE
+    and arrival_timestamp AT TIME ZONE 'UTC' BETWEEN SYSDATE - INTERVAL '180 DAY' AND SYSDATE
     and a_browsinglanguage='tamil'
 GROUP BY a_contentname
 ORDER BY COUNT DESC
@@ -29,8 +29,20 @@ SELECT COUNT(*),a_contentname
     where event_type in ('SongViewStart')
     and a_platform in ('Android','ANDROID')
     and a_subscriberid is not null
-    and arrival_timestamp AT TIME ZONE 'UTC' BETWEEN SYSDATE - INTERVAL '60 DAY' AND SYSDATE
+    and arrival_timestamp AT TIME ZONE 'UTC' BETWEEN SYSDATE - INTERVAL '180 DAY' AND SYSDATE
     and a_browsinglanguage='tamil'
 GROUP BY a_contentname
 ORDER BY COUNT DESC
 
+
+#Count of the top movies downloaded
+
+SELECT COUNT(*),a_contentname
+    from awsma.event
+    where event_type in ('DownloadContentStart')
+    and a_platform in ('Android','ANDROID')
+    and a_subscriberid is not null
+    and arrival_timestamp AT TIME ZONE 'UTC' BETWEEN SYSDATE - INTERVAL '180 DAY' AND SYSDATE
+    and a_browsinglanguage='tamil'
+GROUP BY a_contentname
+ORDER BY COUNT DESC
