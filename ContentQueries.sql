2b674aff267cb89ef6279608aa2bca9bf672118b
Saif Naik <saif.naik@fastfilmz.com>
2018-01-03 16:34:38 +0530

    Query edit
    Making changes for CTR

--- ContentQueries.sql
+++ ContentQueries.sql
@@ -50,3 +50,25 @@ ORDER BY COUNT DESC
 #Clickthroughrate: Within the same session we have to check if a user saw a poseter and playedback the movie
 # MovieClicked event will give content name of the movie and session id. For a given movie we have to 
 # count the number of times a movie was seen and clicked to playback 
+
+SELECT COUNT(*),a_contentname
+    from awsma.event
+    where event_type in ('MovieClicked')
+    and a_platform in ('Android','ANDROID')
+    and a_subscriberid is not null
+    and arrival_timestamp AT TIME ZONE 'UTC' BETWEEN SYSDATE - INTERVAL '180 DAY' AND SYSDATE
+    and a_browsinglanguage='tamil'
+GROUP BY a_contentname
+ORDER BY COUNT DESC
+
+_______________
+
+SELECT COUNT(*),a_contentname
+    from awsma.event
+    where event_type in ('MovieViewStart'|'ContentViewStart')
+    and a_platform in ('Android','ANDROID')
+    and a_subscriberid is not null
+    and arrival_timestamp AT TIME ZONE 'UTC' BETWEEN SYSDATE - INTERVAL '180 DAY' AND SYSDATE
+    and a_browsinglanguage='tamil'
+GROUP BY a_contentname
+ORDER BY COUNT DESC
